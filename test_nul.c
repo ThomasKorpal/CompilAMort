9ae61a5a7136dc82a783bac614e6875e63bd7006
Thomas <thomas@pop-os.localdomain>
2023-04-27 12:27:23 +0200

    test a faire

--- test_nul.c
+++ test_nul.c
@@ -1,11 +1,4 @@
-int start = 0;
-int end = 100;
 void main(){
-    int i, s = start, e = end;
-    int sum = 0;
-    for(i = s; i < e; i = i + 1)
-    {
-        sum = sum + 1;
-    }
-    print("sum: \n", sum, "\n");
+    int a;
+    a = 1+(2+(3+(4+(5+(6+(7+(8+(9+10))))))));
 }

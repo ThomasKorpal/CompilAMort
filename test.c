3ed7e111b6e96f1315455539c6c4ab0d1215d22d
ThomasKorpal <90892641+ThomasKorpal@users.noreply.github.com>
2023-05-07 11:33:47 +0200

    Delete test.c

--- test.c
+++
@@ -1,8 +0,0 @@
-void int bool true false if else while for do print
-+ - * / % > < ~ & | ^ = ; , ( ) { } >> >>> << >= <= == != && ||
-"Coucou"
-"Il dit \"bonjour\"\n"
-15
-variable
-//commentaire non terminal
-
